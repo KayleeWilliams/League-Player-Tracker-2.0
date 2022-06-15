a2574dae93f020802bfe3ddb35234569e2854e1d
Kaylee Williams <65376239+KayleeWilliams@users.noreply.github.com>
2022-06-15 18:33:00 +0100

    Commands now will set on bot start

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import { Client } from "discord.js";
 import { IntentOptions } from "./config/intentOptions";
 import { connectDatabase } from "./database/connectDatabase";
+import { onReady } from "./events/onReady";
 import { onInteraction } from "./events/onInteraction";
-
 
 (async () => {
   const bot = new Client({ intents: IntentOptions });
 
   // When bot is ready
-  bot.on("ready", () => console.log("Tracker online & ready!"));
+  bot.on("ready", async () => await onReady(bot));
 
   // Interaction Handler 
   bot.on("interactionCreate", async (interaction) => await onInteraction(interaction));
